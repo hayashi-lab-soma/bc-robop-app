6411866310ab0fce44bc4352d91b06cfcfc10985
f-tominaga <tommy.tommy.walker@gmail.com>
2021-11-24 01:12:20 +0900

    complete sample input text

--- sample-input-text/src/App.js
+++ sample-input-text/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NameInputText />
     </div>
   );
 }
+
+export const NameInputText = (props) => {
+  const [name, setName] = useState('')
+
+  const handleName = (event) => {
+    setName(event.target.value)
+  }
+
+  return (
+    <input
+      type='text'
+      value={name}
+      onChange={(event) => handleName(event)}
+    />
+  )
+}
+
 
 export default App;
